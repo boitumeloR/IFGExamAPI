308743ed9f021619dd50c6003af30e039d2e9fc5
Boitumelo Rampete <u17120323@tuks.co.za>
2021-01-12 12:14:52 +0200

    Register Auth

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using IFGExamAPI.Models;
+using IFGExamAPI.ViewModels;
 
 namespace IFGExamAPI.Controllers
 {
@@ -11,6 +12,21 @@ namespace IFGExamAPI.Controllers
     public class RegistrationController : ApiController
     {
         IFGExamDBEntities db = new IFGExamDBEntities();
-        // GET: Registration
+        // GET: Registration]
+
+        [Route("RegisterAuth")]
+        [HttpPost]
+
+        public dynamic RegisterAuth(AuthVM vm)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return vm.RegisterUser();
+        }
+
+        public dynamic RegisterLearner()
+        {
+
+        }
     }
 }
--- ViewModels/AuthVM.cs
+++ ViewModels/AuthVM.cs
@@ -1,17 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
+using IFGExamAPI.Models;
 
 namespace IFGExamAPI.ViewModels
 {
     public class AuthVM
     {
+        IFGExamDBEntities db = new IFGExamDBEntities();
         public string EmailAddress { get; set; }
         public string Password { get; set; }
         public string UserSecret { get; set; }
         public string SessionID { get; set; }
         public DateTime SessionExpiry { get; set; }
         public int UserRoleID { get; set; }
+        public int CentreID { get; set; }
+
+        public dynamic RegisterUser()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            
+            if (UserExists() == false)
+            {
+                // Register user
+                UserLogin user = new UserLogin
+                {
+                    EmailAddress = this.EmailAddress,
+                    UserPassword = ComputeSha256Hash(this.Password)
+                };
+
+                try
+                {
+                    db.UserLogins.Add(user);
+                    db.SaveChanges();
+
+                    dynamic toReturn = new ExpandoObject();
+
+                    toReturn.Success = true;
+                    toReturn.Error = null;
+
+                    return toReturn;
+                }
+                catch (Exception e)
+                {
+                    dynamic toReturn = new ExpandoObject();
+
+                    toReturn.Success = false;
+                    toReturn.Error = e.Message;
+                    return toReturn;
+                }
+            }
+            else
+            {
+                // Exists
+
+                dynamic toReturn = new ExpandoObject();
+
+                toReturn.Success = false;
+                toReturn.Error = "A user with that email address already exists.";
+
+                return toReturn;
+            }
+        }
+
+        private bool UserExists()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.UserLogins.Any(zz => zz.EmailAddress == this.EmailAddress);
+        }
+
+        public string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
